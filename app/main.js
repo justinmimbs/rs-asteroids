5b74bb65cb7272f6b5eaf9743406fde42f7be8cd
Justin Mimbs <justin.mimbs@gmail.com>
2019-11-22 18:51:25 -0500

    Export PathList from Rust

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-import init, { Polygon } from './wasm/polygon.js';
+import init, { PathList } from './wasm/polygon.js';
 
 const canvas = document.querySelector('#canvas')
 const context = canvas.getContext('2d');
@@ -6,36 +6,31 @@ const context = canvas.getContext('2d');
 canvas.width = 400;
 canvas.height = 400;
 context.strokeStyle = '#666666';
-context.lineWidth = 0.02;
+context.lineWidth = 1;
 context.lineCap = 'round';
 context.lineJoin = 'round';
 
-function drawPolygon(s, tx, ty, memory, polygon) {
-    const length = polygon.length();
-    const points = new Float64Array(memory.buffer, polygon.points(), length * 2);
-
-    context.setTransform(s, 0, 0, s, tx, ty);
+function drawPolygon(s, tx, ty, length, points) {
     context.beginPath();
-    context.moveTo(points[0], points[1]);
+    context.moveTo(points[0] * s + tx, points[1] * s + ty);
     for (let i = 1; i < length; i += 1) {
-        context.lineTo(points[i * 2], points[i * 2 + 1]);
+        context.lineTo(points[i * 2] * s + tx, points[i * 2 + 1] * s + ty);
     }
     context.closePath();
     context.stroke();
-    context.setTransform(1, 0, 0, 1, 0, 0);
 }
 
 async function main() {
     const { memory } = await init();
-    const hexagon = Polygon.ngon(6);
-
-    drawPolygon(50, 50, 50, memory, hexagon);
-    drawPolygon(30, 110, 110, memory, hexagon);
-    drawPolygon(70, 200, 200, memory, hexagon);
-    drawPolygon(30, 230, 230, memory, hexagon);
-    drawPolygon(20, 350, 350, memory, hexagon);
-
-    hexagon.free();
+    const list = PathList.new();
+    const listLength = list.length();
+    const paths = new Uint32Array(memory.buffer, list.paths(), listLength * 2);
+    for (let i = 0; i < listLength; i += 1) {
+        let length = paths[i * 2];
+        let points = new Float64Array(memory.buffer, paths[i * 2 + 1], length * 2);
+        drawPolygon(40, 50 + i * 100, 100, length, points);
+    }
+    list.free();
 }
 
 main();
--- src/lib.rs
+++ src/lib.rs
@@ -14,33 +14,66 @@ impl Point {
 }
 
 #[wasm_bindgen]
-pub struct Polygon {
-    length: u32,
-    points: Vec<Point>,
-}
+pub struct Path(Vec<Point>);
 
 #[wasm_bindgen]
-impl Polygon {
+impl Path {
     pub fn ngon(n: u32) -> Self {
         let n = n.max(3);
         let angle = (2.0 * PI) / (n as f64);
 
-        Polygon {
-            length: n,
-            points: (0..n)
+        Path(
+            (0..n)
                 .map(|i| {
                     let theta = angle * (i as f64);
                     Point::new(theta.cos(), theta.sin())
                 })
                 .collect(),
-        }
+        )
     }
 
-    pub fn length(&self) -> u32 {
-        self.length
+    pub fn length(&self) -> usize {
+        self.0.len()
     }
 
     pub fn points(&self) -> *const Point {
-        self.points.as_ptr()
+        self.0.as_ptr()
+    }
+}
+
+#[repr(C)]
+pub struct PathRef {
+    length: usize,
+    address: *const Point,
+}
+
+#[wasm_bindgen]
+pub struct PathList {
+    data: Vec<Path>,
+    refs: Vec<PathRef>,
+}
+
+#[wasm_bindgen]
+impl PathList {
+    pub fn new() -> Self {
+        let data = vec![Path::ngon(3), Path::ngon(5), Path::ngon(7)];
+        PathList {
+            refs: data
+                .iter()
+                .map(|path| PathRef {
+                    address: path.points(),
+                    length: path.length(),
+                })
+                .collect(),
+            data,
+        }
+    }
+
+    pub fn length(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn paths(&self) -> *const PathRef {
+        self.refs.as_ptr()
     }
 }
