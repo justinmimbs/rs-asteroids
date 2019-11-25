dcacd70908355377b5c15a2f19635ab4df34cfa6
Justin Mimbs <justin.mimbs@gmail.com>
2019-11-25 13:34:21 -0500

    Render screen with wrap around

--- app/main.js
+++ app/main.js
@@ -1,7 +1,13 @@
 import init, { App } from './wasm/asteroids.js';
 
-const canvas = document.querySelector('#canvas')
+const width = 1200;
+const height = 900;
+
+const canvas = document.createElement('canvas')
 const context = canvas.getContext('2d');
+const screenCanvas = document.querySelector('#canvas')
+const screenContext = screenCanvas.getContext('2d');
+
 let app;
 let memory;
 let time;
@@ -14,13 +20,17 @@ async function main() {
     app = App.new();
     time = Date.now();
 
-    canvas.style.background = '#eeeeee';
-    canvas.width = 1200;
-    canvas.height = 900;
+    canvas.width = 3 * width;
+    canvas.height = 3 * height;
+    context.setTransform(1, 0, 0, 1, width, height);
     context.strokeStyle = '#666666';
     context.lineWidth = 1;
     context.lineCap = 'round';
     context.lineJoin = 'round';
+
+    screenCanvas.style.background = '#eeeeee';
+    screenCanvas.width = width;
+    screenCanvas.height = height;
 
     render();
     requestAnimationFrame(loop);
@@ -40,6 +50,7 @@ function loop() {
 }
 
 function render() {
+    // draw to canvas
     const list = app.view();
     const length = list.length();
     const paths = new Uint32Array(memory.buffer, list.paths(), length * 2);
@@ -47,11 +58,19 @@ function render() {
     const ends = new Uint8Array(memory.buffer, list.ends(), length);
     const points = new Float64Array(memory.buffer, list.points(), list.points_length() * 2);
 
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(-width, -height, 3 * width, 3 * height);
     for (let i = 0; i < length; i += 1) {
         drawPoints(points, paths[i * 2], paths[i * 2 + 1], alphas[i], ends[i] === 1);
     }
     list.free();
+
+    // copy to screenCanvas
+    screenContext.clearRect(0, 0, width, height);
+    for (let row in [ 0, 1, 2 ]) {
+        for (let col in [ 0, 1, 2 ]) {
+            screenContext.drawImage(canvas, col * width, row * height, width, height, 0, 0, width, height);
+        }
+    }
 }
 
 function drawPoints(points, index, length, alpha, isClosed) {
