d8cb2c6d75f166b2cf4b07a068892ac4b280eb76
Justin Mimbs <justin.mimbs@gmail.com>
2020-04-24 13:05:20 -0400

    Change screen colors; add glow effect

--- app/www/main.js
+++ app/www/main.js
@@ -3,8 +3,10 @@ import init, { App } from './wasm/app.js';
 const width = 1200;
 const height = 900;
 
-const canvas = document.createElement('canvas');
-const context = canvas.getContext('2d');
+const drawingCanvas = document.createElement('canvas');
+const drawingContext = drawingCanvas.getContext('2d');
+const effectsCanvas = document.createElement('canvas');
+const effectsContext = effectsCanvas.getContext('2d');
 const screenCanvas = document.querySelector('#canvas');
 const screenContext = screenCanvas.getContext('2d');
 
@@ -20,15 +22,18 @@ async function main() {
     app = App.new();
     time = Date.now();
 
-    canvas.width = 3 * width;
-    canvas.height = 3 * height;
-    context.setTransform(1, 0, 0, 1, width, height);
-    context.strokeStyle = '#666666';
-    context.lineWidth = 1;
-    context.lineCap = 'round';
-    context.lineJoin = 'round';
+    drawingCanvas.width = 3 * width;
+    drawingCanvas.height = 3 * height;
+    drawingContext.strokeStyle = '#EAF9FF';
+    drawingContext.lineWidth = 1.5;
+    drawingContext.lineCap = 'round';
+    drawingContext.lineJoin = 'round';
 
-    screenCanvas.style.background = '#eeeeee';
+    effectsCanvas.width = width;
+    effectsCanvas.height = height;
+    effectsContext.filter = 'blur(17px)';
+    effectsContext.globalAlpha = 0.5;
+
     screenCanvas.width = width;
     screenCanvas.height = height;
 
@@ -103,7 +108,7 @@ function loop() {
 }
 
 function draw() {
-    // draw to canvas
+    // render
     const list = app.render();
     const length = list.length();
     const paths = new Uint32Array(memory.buffer, list.paths(), length * 2);
@@ -111,22 +116,30 @@ function draw() {
     const ends = new Uint8Array(memory.buffer, list.ends(), length);
     const points = new Float64Array(memory.buffer, list.points(), list.points_length() * 2);
 
-    context.clearRect(-width, -height, 3 * width, 3 * height);
+    // drawing
+    drawingContext.clearRect(-width, -height, 3 * width, 3 * height);
     for (let i = 0; i < length; i += 1) {
-        drawPoints(points, paths[i * 2], paths[i * 2 + 1], alphas[i], ends[i] === 1);
+        drawPoints(drawingContext, points, paths[i * 2], paths[i * 2 + 1], alphas[i], ends[i] === 1);
     }
     list.free();
 
-    // copy to screenCanvas
+    // effects
+    effectsContext.clearRect(0, 0, width, height);
+    effectsContext.drawImage(screenCanvas, 0, 0, width, height);
+
+    // screen
     screenContext.clearRect(0, 0, width, height);
+    // effects -> screen
+    screenContext.drawImage(effectsCanvas, 0, 0, width, height);
+    // drawing -> screen
     for (let row in [ 0, 1, 2 ]) {
         for (let col in [ 0, 1, 2 ]) {
-            screenContext.drawImage(canvas, col * width, row * height, width, height, 0, 0, width, height);
+            screenContext.drawImage(drawingCanvas, col * width, row * height, width, height, 0, 0, width, height);
         }
     }
 }
 
-function drawPoints(points, index, length, alpha, isClosed) {
+function drawPoints(context, points, index, length, alpha, isClosed) {
     context.beginPath();
     context.globalAlpha = alpha;
     context.moveTo(points[index * 2], points[index * 2 + 1]);
